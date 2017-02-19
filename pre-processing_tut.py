93c56fd0445cc707fa29bf533e0f2f3fab9dd7fb
Stephen M <famousshooter98@gmail.com>
2017-02-19 17:18:15 -0500

    This is my update before restarting my computer on 2/19/17

--- pre-processing_tut.py
+++ pre-processing_tut.py
@@ -14,5 +14,27 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Some Constants
 INPUT_FOLDER = 'E:/BE400_Data/'
-#patients = os.listdir(INPUT_FOLDER)
-#patients.sort()
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
+
+# Load the scans in a given folder path
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0]SliceLocation - slices[1].SliceLocation)
+    
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+# okay, defin a new function
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # convert to in16 (from sometimes int16)
+
+    image = image.astype(np.int16)
+    
