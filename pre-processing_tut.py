85426128630a90a8b3568e23259c0a47f6809a15
Stephen M <famousshooter98@gmail.com>
2017-02-19 20:17:21 -0500

    Okay, this script officially runs

--- pre-processing_tut.py
+++ pre-processing_tut.py
@@ -13,7 +13,7 @@ from skimage import measure, morphology
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Some Constants
-INPUT_FOLDER = 'E:/BE400_Data/'
+INPUT_FOLDER = 'C:/Users/stephen_GAME/Google Drive/UB documents/Spring 2017/BE 400/kaggle_group/sample_images/'  # Note, this is for sample images
 patients = os.listdir(INPUT_FOLDER)
 patients.sort()
 
@@ -24,7 +24,7 @@ def load_scan(path):
     try:
         slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
     except:
-        slice_thickness = np.abs(slices[0]SliceLocation - slices[1].SliceLocation)
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
     
     for s in slices:
         s.SliceThickness = slice_thickness
@@ -37,4 +37,40 @@ def get_pixels_hu(slices):
     # convert to in16 (from sometimes int16)
 
     image = image.astype(np.int16)
+
+# This was copied directly from the website
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16), 
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
     
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+        
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+        
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+            
+        image[slice_number] += np.int16(intercept)
+    
+    return np.array(image, dtype=np.int16)
+
+# Now we will try to get an actual image
+first_patient = load_scan(INPUT_FOLDER + patients[0])
+first_patient_pixels = get_pixels_hu(first_patient)
+plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.show()
+
+# Show some slice in the middle
+plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
+plt.show()
