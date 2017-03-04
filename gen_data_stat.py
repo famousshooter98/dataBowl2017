406452ad68a13b2876825125d5166516ba87bd12
Stephen M <famousshooter98@gmail.com>
2017-03-04 12:28:12 -0500

    I added a general definitions file and a script for general data statistics

---
+++ gen_data_stat.py
@@ -0,0 +1,8 @@
+# First pass data general statistics
+
+import definitions
+
+# Lets see what errors are thrown
+INPUT_FOLDER = 'C:/Users/stephen_GAME/Google Drive/UB documents/Spring 2017/BE 400/kaggle_group/sample_images/'  # Note, this is for sample images
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
