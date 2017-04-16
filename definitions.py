64000981929b169cca1097385bf90b96dab5771d
Stephen M <famousshooter98@gmail.com>
2017-04-16 16:04:15 -0400

    Some minor changes

--- definitions.py
+++ definitions.py
@@ -6,13 +6,20 @@ import dicom
 import scipy.ndimage
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
+from sklearn.cluster import KMeans
 
+from skimage.transform import resize
 from skimage import measure
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 # Newer imports
 from skimage.morphology import binary_closing, disk, binary_erosion, ball
 from skimage.segmentation import clear_border
 from skimage.filters import roberts
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.tools import FigureFactory as FF
+from plotly.graph_objs import *
+
 
 
 # Load the scans in given folder path
@@ -252,7 +259,6 @@ def Nplot_3d(image, threshold=-300):
     p = p[:,:,::-1]
     
     verts, faces = measure.marching_cubes(p, threshold)
-<<<<<<< HEAD
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
@@ -269,28 +275,57 @@ def Nplot_3d(image, threshold=-300):
 
     plt.show()  
 
-def plot_2d(scan):
+def plot_ct_scan(scan):
     f, plots = plt.subplots(int(scan.shape[0] / 20) + 1, 4, figsize=(25, 25))
     for i in range(0, scan.shape[0], 5):
         plots[int(i / 20), int((i % 20) / 5)].axis('off')
-        plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone)     
-=======
+        plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone) 
+        
+def make_mesh(image, threshold=-300, sstep_size=1):
 
+
+    p = image.transpose(2,1,0)
+    
+
+    verts, faces, norm, val = measure.marching_cubes(p, threshold, step_size=sstep_size, allow_degenerate=True) 
+    return verts, faces
+
+def plotly_3d(verts, faces):
+    x,y,z = zip(*verts) 
+    
+
+    
+    # Make the colormap single color since the axes are positional not intensity. 
+#    colormap=['rgb(255,105,180)','rgb(255,255,51)','rgb(0,191,255)']
+    colormap=['rgb(236, 236, 212)','rgb(236, 236, 212)']
+    
+    fig = FF.create_trisurf(x=x,
+                        y=y, 
+                        z=z, 
+                        plot_edges=False,
+                        colormap=colormap,
+                        simplices=faces,
+                        backgroundcolor='rgb(64, 64, 64)',
+                        title="Interactive Visualization")
+    iplot(fig)
+
+def plt_3d(verts, faces):
+
+    x,y,z = zip(*verts) 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
 
     # Fancy indexing: `verts[faces]` to generate a collection of triangles
-    mesh = Poly3DCollection(verts[faces], alpha=0.1)
-    face_color = [0.5, 0.5, 1]
+    mesh = Poly3DCollection(verts[faces], linewidths=0.05, alpha=1)
+    face_color = [1, 1, 0.9]
     mesh.set_facecolor(face_color)
     ax.add_collection3d(mesh)
 
-    ax.set_xlim(0, p.shape[0])
-    ax.set_ylim(0, p.shape[1])
-    ax.set_zlim(0, p.shape[2])
-
-    plt.show()  
->>>>>>> 27bf9a2c0d4131e09e34377fc21190fc993cc649
+    ax.set_xlim(0, max(x))
+    ax.set_ylim(0, max(y))
+    ax.set_zlim(0, max(z))
+    ax.set_axis_bgcolor((0.7, 0.7, 0.7))
+    plt.show()
 
 ## UNET Candidate Detection
 #
